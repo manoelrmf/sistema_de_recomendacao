9e14268a8ffdc09f960b5d458143cec73da37a5d
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-03-24 16:36:33 -0300

    capturando dados do formulário

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,84 @@
 $(document).ready(function () {
-   
-    
+  /*
+  JSON.stringfy(objeto): para armazenar os dados, utilizaremos o formato JSON e esta função transforma um objeto em string com sintaxe adequado ao JSON.
+  JSON.parse(objeto): já a função parse transforma um item no formato JSON no seu formato original.
+  */
 
-    //requestt a api
-/* function getData(ajaxurl) { 
-    return $.ajax({
-      url: ajaxurl,
-      type: 'GET',
+  loadPageQuests();
+
+  $('#home').click(function (e) {
+    loadPageForm("Por favor, informe seus dados", "usuario")
+  });
+
+
+  function loadPageForm(title ,userType) {
+    $('main').load("form.html", "data", function (response, status, request) {
+      this;
+      loadFormUsuario(title)
+
+      $('#formButton').click(function (e) {
+        e.preventDefault();
+        var name = $('#nomeID').val();
+        var sexo = $('#sexoID').val();
+
+        var usuarioObj = {
+          "nome": name,
+          "sexo": sexo,
+          "tipo": userType
+        };
+
+        setValueLocalStorage(userType, JSON.stringify(usuarioObj))
+
+        if(userType === "usuario")
+          loadPageForm("Por favor, informe os dados do seu parceiro", "parceiro")
+
+        if(userType === "parceiro")
+          loadPageQuests()
+      });
     });
-};
-  
-  async function test() {
-    try {
-      const data = await getData('http://www.omdbapi.com/?t=The+Prestige&apikey=5eb9dd68')
-      setTitle(data.Title)
-    } catch(err) {
-      console.log(err);
+  }
+
+
+  function loadFormUsuario(title) {
+    $('#formTitle').text(title);
+  }
+
+  function loadPageQuests() {
+    $('main').load("quests.html", "data", function (response, status, request) {
+      this;
+
+    });
+  }
+
+  function setValueLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+
+  }
+
+
+  //requestt a api
+  /* function getData(ajaxurl) { 
+      return $.ajax({
+        url: ajaxurl,
+        type: 'GET',
+      });
+  };
+    
+    async function test() {
+      try {
+        const data = await getData('http://www.omdbapi.com/?t=The+Prestige&apikey=5eb9dd68')
+        setTitle(data.Title)
+      } catch(err) {
+        console.log(err);
+      }
     }
-  }
-
-  function setTitle(title){
-      $(".teste").text(title)
-  }
   
-  test();
-
-*/
+    function setTitle(title){
+        $(".teste").text(title)
+    }
+    
+    test();
+  
+  */
 
 });
