f098e7b82f3078a499ea4f8b44b3d23f9fa9b3a2
Manoel Ribeiro <manoel.inf1.ep@gmail.com>
2020-03-24 14:43:47 -0300

    Commit inicial da estrutura do projeto

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+   
+    
+
+    //requestt a api
+/* function getData(ajaxurl) { 
+    return $.ajax({
+      url: ajaxurl,
+      type: 'GET',
+    });
+};
+  
+  async function test() {
+    try {
+      const data = await getData('http://www.omdbapi.com/?t=The+Prestige&apikey=5eb9dd68')
+      setTitle(data.Title)
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  function setTitle(title){
+      $(".teste").text(title)
+  }
+  
+  test();
+
+*/
+
+});
